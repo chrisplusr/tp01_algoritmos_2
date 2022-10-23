2d1a0dbb882b90b5a14682c9089bb68709c67db2
djalv <alvaroneto98@protonmail.com>
2022-10-23 02:55:27 -0300

    Refatoração de codigo e comentarios

--- segments_intersect.py
+++ segments_intersect.py
@@ -1,38 +1,15 @@
 from functools import cmp_to_key
 import red_black_tree as rbt
-from point import Point
+import geometry_tools as gt
 
-def on_segment(Pi, Pj, Pk):
-    if(min(Pi.x, Pj.x) < Pk.x and Pk.x < max(Pi.x, Pj.x)) and (min(Pi.y, Pj.y) < Pk.y and Pk.y < max(Pi.y, Pj.y)):
-        return True
-    else:
-        return False
-
-def intersect(p1, p2, p3, p4):
-    d1 = direction(p3, p4, p1)
-    d2 = direction(p3, p4, p2)
-    d3 = direction(p1, p2, p3)
-    d4 = direction(p1, p2, p4)
-
-    if((d1 > 0 and d2 < 0) or (d1 < 0 and d2 > 0)) and ((d3 > 0 and d4 < 0) or (d3 < 0 and d4 > 0)):
-        return True
-    elif d1 == 0 and on_segment(p3, p4, p1):
-        return True
-    elif d2 == 0 and on_segment(p3, p4, p2):
-        return True
-    elif d3 == 0 and on_segment(p1, p2, p3):
-        return True
-    elif d4 == 0 and on_segment(p1, p2, p4):
-        return True    
-    else:
-        return False
-
-def direction(Pi, Pj, Pk):
-    r1 = Pj - Pi
-    r2 = Pk - Pi
-    return (r1.x * r2.y) - (r1.y * r2.x)
 
 def cmp_points(p1, p2):
+    '''
+    Compara qual ponto esta mais a esquerda
+    se ambos tiverem coodernada x iguais
+    compara o tipo de ponto que cada um é, se é final = 1
+    ou inicial = 0, se os tipos forem iguais compara qual é o mais baixo
+    '''
     if p1.x < p2.x:
         return -1
     elif p1.x > p2.x:
@@ -50,39 +27,39 @@ def cmp_points(p1, p2):
                 return 1
 
 def any_segments_intersect(S):
-    T = rbt.RedBlackTree()
+    '''
+    Verifica se quaisquer dois segmentos de um conjunto
+    se interseptam.
+    '''
+    T = rbt.RedBlackTree() # Inicializa a arvore rubro-negra
 
-    sorted_segments = sorted(S, key=cmp_to_key(cmp_points))
-    #for i in sorted_segments:
-        #print(i, i.p_type)
-    sorted_segments = map(lambda x: rbt.Node(x), sorted_segments)
+    sorted_segments = sorted(S, key=cmp_to_key(cmp_points)) # Ordena os pontos usando a funçao de comparação
+    sorted_segments = map(lambda x: rbt.Node(x), sorted_segments) # Faz com que os pontos sejam nós de uma arvore
     
-    for i in sorted_segments: 
-        if i.key.p_type == 0:
+    for i in sorted_segments: # Percorre os segmentos ordenados
+        if i.key.p_type == 0: # Se o ponto do segmento for inicial
             
-            T.insert(i)
+            T.insert(i) # Insere o segmento na arvore
             x = T.search(i)
-            pred = T.predecessor(x)
-            suc = T.successor(x)
-            '''
-            if pred:
-                print(x.key, x.key.other_end, pred.key, pred.key.other_end)
-            if suc:
-                print(x.key, x.key.other_end, pred.key, suc.key.other_end)
-            '''
-            if (pred and intersect(i.key, i.key.other_end, pred.key, pred.key.other_end)) or (suc and intersect(i.key, i.key.other_end, suc.key, suc.key.other_end)):
+            pred = T.predecessor(x) # Pega o segmento q esta acima de i
+            suc = T.successor(x) # Pega o segmento q esta abaixo de i
+            
+            # Verifica se existem os segmentos que estão acima de i ou abaixo de i e interceptam i
+            if (pred and gt.intersect(i.key, i.key.other_end, pred.key, pred.key.other_end)) or (suc and gt.intersect(i.key, i.key.other_end, suc.key, suc.key.other_end)):
                 return True
         
-        if i.key.p_type == 1:
+        if i.key.p_type == 1: # Se o ponto do segmento for final
             
             x = T.search(i)
-            pred = T.predecessor(x)
-            suc = T.successor(x)
+            pred = T.predecessor(x) # Pega o segmento q esta acima de i
+            suc = T.successor(x) # Pega o segmento q esta abaixo de i
             
+            # Verifica se existem os segmentos que estão acima de i ou abaixo de i e se interceptam
             if pred and suc:
-                if intersect(pred.key, pred.key.other_end, suc.key, suc.key.other_end):
+                if gt.intersect(pred.key, pred.key.other_end, suc.key, suc.key.other_end):
                     return True
             
+            # Deleta o segmento i
             T.delete(i)
     
     return False
